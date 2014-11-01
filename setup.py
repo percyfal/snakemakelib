830380ddb4547bb493ba3134303f51529aee818c
Per Unneberg <per.unneberg@scilifelab.se>
2014-11-01 09:02:07 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2013 Per Unneberg
+from setuptools import setup, find_packages
+import os
+import glob
+
+setup(name = "snakemakelib",
+      version = "0.1.0",
+      author = "Per Unneberg",
+      author_email = "per.unneberg@scilifelab.se",
+      description = "Snakemake rule library with additional utilities",
+      license = "MIT",
+      scripts = glob.glob('scripts/*.py'),
+      install_requires = [
+        "pyyaml",
+        "matplotlib>=1.2.1",
+        ## Required for testing
+        "nose",
+        ],
+      test_suite = 'nose.collector',
+      packages=find_packages(exclude=['ez_setup', 'test*']),
+      namespace_packages = [
+        'snakemakelib',
+        'snakemakelib.ext',
+        ],
+      package_data = {
+        'snakemakelib':[
+            'snakefiles/*',
+            ]},
+      )
+
+os.system("git rev-parse --short --verify HEAD > ~/.snakemakelib_version")

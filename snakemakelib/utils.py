7e1d3099ea5b17ca61185a40da7234ae7af84334
Per Unneberg <per.unneberg@scilifelab.se>
2015-03-04 12:31:53 +0100

    * rules/bio/ngs/align/star.rules: update indexing rule and add alignment rule * rules/comp/settings.rules: add compression section * snakemakelib/utils.py: add function to check for compression

--- snakemakelib/utils.py
+++ snakemakelib/utils.py
@@ -1,6 +1,13 @@
 # Copyright (c) 2014 Per Unneberg
+import re
 from datetime import datetime
+from snakemakelib.config import get_sml_config
 
 def utc_time():
     """Make an utc_time with appended 'Z'"""
     return str(datetime.utcnow()) + 'Z'
+
+def is_compressed(f):
+    """Ascertain whether a file is compressed or not"""
+    cfg = get_sml_config('comp.settings')
+    return not re.search(cfg['compression']['re'], f) is None

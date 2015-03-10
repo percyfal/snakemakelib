0e294590f72924d9e0aa1846c6285f4094aac931
Per Unneberg <per.unneberg@scilifelab.se>
2015-03-10 08:05:38 +0100

    Add utility functions for read group handling

--- snakemakelib/bio/ngs/utils.py
+++ snakemakelib/bio/ngs/utils.py
@@ -1,10 +1,47 @@
 # Copyright (C) 2015 by Per Unneberg
 import re
+import os
 from snakemakelib.config import get_sml_config
 from snakemakelib.utils import isoformat
 
 sml_config = get_sml_config()
 
+# SAM format specification
+# @RG Read group. Unordered multiple @RG lines are allowed.
+
+#   ID* Read group identifer. Each @RG line must have a unique ID. The
+#   value of ID is used in the RG tags of alignment records. Must be
+#   unique among all read groups in header section. Read group IDs may
+#   be modied when merging SAM les in order to handle collisions.
+
+# CN Name of sequencing center producing the read.
+# DS Description.
+# DT Date the run was produced (ISO8601 date or date/time).
+
+# FO Flow order. The array of nucleotide bases that correspond to the
+# nucleotides used for each ow of each read. Multi-base ows are
+# encoded in IUPAC format, and non-nucleotide ows by various other
+# characters. Format: /\*|[ACMGRSVTWYHKDBN]+/
+
+# KS The array of nucleotide bases that correspond to the key sequence of each read.
+
+# LB Library.
+
+# PG Programs used for processing the read group.
+
+# PI Predicted median insert size.
+
+# PL Platform/technology used to produce the reads. Valid values:
+# CAPILLARY, LS454, ILLUMINA, SOLID, HELICOS, IONTORRENT, ONT, and
+# PACBIO.
+
+# PM Platform model. Free-form text providing further details of the platform/technology used.
+
+# PU Platform unit (e.g. flowcell-barcode.lane for Illumina or slide for SOLiD). Unique identifier.
+
+# SM Sample. Use pool name where a pool is being sequenced.
+
+_read_group_dict =  {'ID':'identifier', 'CN':'center', 'DS':'description', 'DT':'date', 'FO':'floworder', 'KS':'keysequence', 'LB':'library', 'PG':'program', 'PI':'insertsize', 'PL': }
 def read_group_from_str(s):
     """Generate read group values from a string.
 
@@ -32,3 +69,19 @@ def read_group_from_str(s):
     if "id" in list(d):
         d["id"] = s
     return d
+
+def find_files(path, re_str):
+    """Find files in path that comply with a regular expression.
+
+    Args:
+      path:   path to search
+      re_str: regular expression string
+
+    Returns:
+      flist: list of file names
+    """
+    r = re.compile(re_str)
+    flist = []
+    for root, dirs, files in os.walk(path):
+        flist += [os.path.join(root, x) for x in files if r.match(x)]
+    return sorted(flist)
--- test/test_utils.py
+++ test/test_utils.py
@@ -4,8 +4,9 @@ import unittest
 import logging
 from snakemakelib.config import init_sml_config
 from snakemakelib.report.utils import Template
-from snakemakelib.bio.ngs.utils import read_group_from_str
+from snakemakelib.bio.ngs.utils import read_group_from_str, find_files
 from snakemakelib.utils import isoformat
+from snakemake.io import glob_wildcards
 
 logger = logging.getLogger(__name__)
 
@@ -43,15 +44,18 @@ class TestBioNgsUtils(unittest.TestCase):
     """Test snakemakelib.bio.ngs.utils functions"""
     def setUp(self):
         """Setup test fixtures"""
-        cfg = {
+        self.cfg = {
             'bio.ngs.settings' : {
-                'run_id_re' : (("platform-unit", "date", "_", "sample"), "([0-9])_([0-9]+)_([A-Z0-9]+XX)_(P[0-9]+_[0-9]+)"), 
-                'read_group_keys' : ("id", "sample", "library", "description", "platform-unit", "center", "date", "platform"),
+                'run_id_re' : (("punit", "date", "_", "sample"), "([0-9])_([0-9]+)_([A-Z0-9]+XX)_(P[0-9]+_[0-9]+)"), 
+                'read_group_keys' : ("id", "sample", "library", "description", "punit", "center", "date", "platform"),
                 'center' : 'mycenter',
                 'platform' : 'Illumina',
+                'fastq_suffix' : ".fastq.gz",
+                'read1_label' : "_1",
+                'read2_label' : "_2",
             },
         }
-        init_sml_config(cfg)
+        init_sml_config(self.cfg)
 
     def tearDown(self):
         init_sml_config({})
@@ -60,8 +64,30 @@ class TestBioNgsUtils(unittest.TestCase):
         """Test creating read group information from strings"""
         d = read_group_from_str("2_120924_AC003CCCXX_P001_102")
         self.assertEqual(d['date'], "2012-09-24")
-        self.assertEqual(d['platform-unit'], "2")
+        self.assertEqual(d['punit'], "2")
         self.assertEqual(d['id'], "2_120924_AC003CCCXX_P001_102")
+
+    def test_find_files(self):
+        """Test finding files"""
+        read1_str = self.cfg['bio.ngs.settings']["run_id_re"][1] + self.cfg['bio.ngs.settings']["read1_label"] + self.cfg['bio.ngs.settings']["fastq_suffix"] + "$"
+        read2_str = self.cfg['bio.ngs.settings']["run_id_re"][1] + self.cfg['bio.ngs.settings']["read2_label"] + self.cfg['bio.ngs.settings']["fastq_suffix"] + "$"
+        flist = find_files("../data/projects/J.Doe_00_01", read1_str)
+        self.assertEqual(len(flist), 3)
+        self.assertEqual(flist[1], '../data/projects/J.Doe_00_01/P001_101/121015_BB002BBBXX/1_121015_BB002BBBXX_P001_101_1.fastq.gz')
+        readpairs = list(zip(flist, find_files("../data/projects/J.Doe_00_01", read2_str)))
+        self.assertEqual(len(readpairs), 3)
+        self.assertEqual(len(readpairs[0]), 2)
+        self.assertEqual(readpairs[1][0], '../data/projects/J.Doe_00_01/P001_101/121015_BB002BBBXX/1_121015_BB002BBBXX_P001_101_1.fastq.gz')
+
+    def test_sm_wildcard(self):
+        read1_str = "../data/projects/J.Doe_00_01/P001_101/121015_BB002BBBXX/" + self.cfg['bio.ngs.settings']["run_id_re"][1] + self.cfg['bio.ngs.settings']["read1_label"] + self.cfg['bio.ngs.settings']["fastq_suffix"] + "$"
+        print (read1_str)
+        gw = glob_wildcards(read1_str)
+        print (gw)
+        read2_str = "../data/projects/J.Doe_00_01/P001_101/121015_BB002BBBXX/{platform_unit}_{fc}_{sample}_2.fastq.gz"
+        gw = glob_wildcards(read2_str)
+        print (gw)
+        
 
 class TestUtils(unittest.TestCase):
     """Test snakemakelib.utils functions"""

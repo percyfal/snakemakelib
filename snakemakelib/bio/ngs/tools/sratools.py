dfcd519d38650c2f8fab2d98361e26a698cb4db0
Per Unneberg <per.unneberg@scilifelab.se>
2015-03-20 11:54:26 +0100

    * rules/bio/ngs/qc/picard.rules: use PICARD_HOME * rules/bio/ngs/tools/sratools.rules: remove obsolete rules as the process can be much simplified * rules/comp/settings.rules: add support for VENV environment variables * snakemakelib/bio/ngs/tools/sratools.py: add function for reading metadata

---
+++ snakemakelib/bio/ngs/tools/sratools.py
@@ -0,0 +1,30 @@
+# Copyright (C) 2015 by Per Unneberg
+
+import os
+import csv
+from snakemakelib.log import get_logger
+
+logger = get_logger()
+
+def get_metadata_list(metadata_file):
+    """
+    Read an SRA project file and return entries as a list. Will issue a
+    warning if file does not exists.
+
+    Args: 
+      metadata - file name
+
+    Returns:
+      list of file contents where every item is a dictionary
+
+    """
+    metadata_list = []
+    if os.path.exists(metadata_file):
+        with open(metadata_file, "r") as fh:
+            reader = csv.DictReader(fh.readlines())
+        metadata_list = [row for row in reader]
+    else:
+        import time
+        logger.warn ("no metadata file '{metadata}' found;\n\nplease initiate analysis by running 'snakemake {metadata}'".format(metadata=metadata_file))
+        time.sleep(3)
+    return metadata_list

e4a46fa9dde94592e12d1c71856fdc02db051388
Per Unneberg <per.unneberg@scilifelab.se>
2015-04-18 19:40:25 +0200

    * requirements.txt: add pysam * rules/bio/ngs/align/bowtie.rules: fix bowtie aligment rule * rules/bio/ngs/enrichment/peakseq.rules: add docstring * rules/bio/ngs/qc/picard.rules: merge: do copy instead of symlink * rules/settings.rules: add temp_files * snakemakelib/io.py: only include large file types in temp_files

--- snakemakelib/io.py
+++ snakemakelib/io.py
@@ -1,14 +1,33 @@
 # Copyright (C) 2015 by Per Unneberg
 import os
+import re
+import copy
 from snakemakelib.log import LoggerManager
 
 smllogger = LoggerManager().getLogger(__name__)
 
-def set_temp_output(workflow, rules=[]):
-    """Given list of rules, set their corresponding outputs to temporary outputs"""
+DEFAULT_TEMP_FILES = ['.bam', '.gz', '.zip', '.bigWig']
+
+def set_temp_output(workflow, rules=[], temp_files=[]):
+    """Given list of rules, set their corresponding outputs to temporary outputs.
+
+    Args:
+      workflow: snakemake workflow
+      rules: list of rules
+      temp_files: file extensions 
+
+    Returns:
+      None
+    """
+    sfx = DEFAULT_TEMP_FILES + temp_files
+    r = re.compile("|".join("{}$".format(x) for x in sfx))
     for rule in rules:
         smllogger.info("setting output in rule {rule} to temporary".format(rule=rule))
         try:
-            workflow._rules[rule].temp_output = set(workflow._rules[rule].output)
+            output = copy.copy(workflow._rules[rule].output)
+            for o in workflow._rules[rule].output:
+                if r.search(o) is None:
+                    output.remove(o)
+            workflow._rules[rule].temp_output = set(output)
         except:
             smllogger.warn("failed to set output in rule {rule} to temporary".format(rule=rule))

2c394b7188fe524b028939630b58550faabb7118
Per Unneberg <per.unneberg@scilifelab.se>
2015-05-19 10:35:14 +0200

    Add versioneer
    * setup.py: update to accomodate versioneer

--- setup.py
+++ setup.py
@@ -2,6 +2,13 @@
 from setuptools import setup, find_packages
 import os
 import glob
+import versioneer
+
+versioneer.VCS = 'git'
+versioneer.versionfile_source = 'snakemakelib/_version.py'
+versioneer.versionfile_build = 'snakemakelib/_version.py'
+versioneer.tag_prefix = '' # tags are like 1.2.0
+versioneer.parentdir_prefix = 'snakemakelib-' # dirname like 'myproject-1.2.0'
 
 try:
     with open("requirements.txt", "r") as fh:
@@ -9,24 +16,27 @@ try:
 except IOError:
     install_requires = []
     
-setup(name = "snakemakelib",
-      version = "0.1.0",
-      author = "Per Unneberg",
-      author_email = "per.unneberg@scilifelab.se",
-      description = "Snakemake rule library with additional utilities",
-      license = "MIT",
-      scripts = glob.glob('scripts/*.py'),
-      install_requires = install_requires,
-      test_suite = 'nose.collector',
-      packages=find_packages(exclude=['ez_setup', 'test*']),
-      namespace_packages = [
-          'snakemakelib',
-      ],
-      package_data = {
-          'snakemakelib':[
-              'data/*',
-              'rules/*',
-              'examples/*',
-              'templates/*',
-          ]},
-  )
+setup(
+    name = "snakemakelib",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author = "Per Unneberg",
+    author_email = "per.unneberg@scilifelab.se",
+    description = "Snakemake rule library with additional utilities",
+    license = "MIT",
+    url = "http://github.com/percyfal/snakemakelib',
+    scripts = glob.glob('scripts/*.py'),
+    install_requires = install_requires,
+    test_suite = 'nose.collector',
+    packages=find_packages(exclude=['ez_setup', 'test*']),
+    namespace_packages = [
+        'snakemakelib',
+    ],
+    package_data = {
+        'snakemakelib':[
+            'data/*',
+            'rules/*',
+            'examples/*',
+            'templates/*',
+    ]},
+)
--- snakemakelib/__init__.py
+++ snakemakelib/__init__.py
@@ -1,3 +1,7 @@
 # Copyright (c) 2013 Per Unneberg
 
 __import__('pkg_resources').declare_namespace(__name__)
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions

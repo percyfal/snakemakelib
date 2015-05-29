9dedfbd51aa60e3fb8663801e89965488163400f
Per Unneberg <per.unneberg@scilifelab.se>
2015-05-29 17:10:05 +0200

    Update sphinx settings
    * requirements.txt: require sphinx 1.3
    * rules/bio/ngs/settings.rules: add global settings
    * snakemakelib/rules.py: add input rule modification
    * sphinx/source/conf.py: add napoleon extension for Google-style docstrings

--- snakemakelib/rules.py
+++ snakemakelib/rules.py
@@ -8,16 +8,17 @@ from snakemakelib.log import LoggerManager
 smllogger = LoggerManager().getLogger(__name__)
 
 def create_rule_from_existing(name, template, workflow, **kw):
-    """
+    """Create rule from a template.
+    
     Create rule from existing rule and add it to the workflow. By
     passing keyword arguments it is also possible to update/modify the
-    output and/or params.
+    input, output and/or params.
 
     Args:
         name (str): name of new rule
         template (str): name of existing template rule
         workflow (:class:`Workflow <snakemake.workflow.Workflow>`): snakemake workflow
-        kw (dict): keyword argument for updating output and/or params
+        kw (dict): keyword argument for updating input, output and/or params
         
     Returns:
         None
@@ -35,6 +36,9 @@ def create_rule_from_existing(name, template, workflow, **kw):
         assert type(kw['output']) is tuple, "output argument must be a tuple of type (tuple, dict)"
         rule._output = OutputFiles()
         workflow._rules[name].set_output(*kw['output'][0], **kw['output'][1])
+    if kw.get('input'):
+        assert type(kw['input']) is tuple, "input argument must be a tuple of type (tuple, dict)"
+        workflow._rules[name].set_input(*kw['input'][0], **kw['input'][1])
     if kw.get('params'):
         assert type(kw['params']) is tuple, "params argument must be a tuple of type (tuple, dict)"
         workflow._rules[name].set_params(*kw['params'][0], **kw['params'][1])

5f71ab0838961276be9d617611a3b1e2d58c362b
Per Unneberg <per.unneberg@scilifelab.se>
2015-08-26 12:48:36 +0200

    Misc fixes
    - remove test doc files
    - readd load_sml_config

--- snakemakelib/config.py
+++ snakemakelib/config.py
@@ -8,6 +8,34 @@ from collections import Mapping
 
 smllogger = LoggerManager().getLogger(__name__)
 
+def load_sml_config(config, cfg_file=None):
+    """Load snakemakelib configuration file(s).
+    Will search for configuration files in following order:
+    
+    1. ~/.smlconf.yaml - a personal site-wide configuration file
+    2. ./smlconf.yaml - a standard configuration file residing in the
+        same directory as the Snakefile
+    3. cfg_file, if provided
+    Args:
+      config (dict): snakemake configuration object
+      cfg_file (str): custom configuration file to load
+    Returns:
+      config (dict): updated configuration
+    """
+    for fn in [os.path.join(os.getenv("HOME"), ".smlconf.yaml"),
+               os.path.join(os.curdir, "smlconf.yaml"),
+               cfg_file]:
+        if (fn is None):
+            continue
+        if not os.path.exists(fn):
+            continue
+        smllogger.info("Loading configuration from {}".format(fn))
+        with open(fn, "r") as fh:
+            cfg = yaml.load(fh)
+        smllogger.info("Read configuration from {}".format(fn))
+        config = update_config(config, cfg)
+    return config
+
 
 def sml_path():
     return os.path.dirname(__file__)
